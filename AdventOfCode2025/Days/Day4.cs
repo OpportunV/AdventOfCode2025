511b1ece868597f4e1dcb4aa3fcd6aee10c945a2
OpportunV <RsTGear@gmail.com>
2025-12-04 22:32:00 +0300

    Day 4 part 2 speedup

--- AdventOfCode2025/Days/Day4.cs
+++ AdventOfCode2025/Days/Day4.cs
@@ -8,8 +8,8 @@ public class Day4 : Day
 {
     private readonly Grid<char> _grid;
     private const int Amount = 4;
-    private const char PaperRoll = '@';
     private const char Empty = '.';
+    private const char PaperRoll = '@';
 
     public Day4()
     {
@@ -18,7 +18,7 @@ public class Day4 : Day
 
     public override string Part1()
     {
-        return GetRemovableRolls()
+        return GetRemovableRolls(_grid.Flatten().Where(RollsFilter))
             .Count()
             .ToString();
     }
@@ -26,23 +26,30 @@ public class Day4 : Day
     public override string Part2()
     {
         var counter = 0;
-        List<GridItem<char>> removableRolls;
+        var removableRolls = GetRemovableRolls(_grid.Flatten().Where(RollsFilter)).ToList();
         do
         {
-            removableRolls = GetRemovableRolls().ToList();
             counter += removableRolls.Count;
             removableRolls.ForEach(removableRoll => _grid[removableRoll.Pos] = Empty);
+            var candidates = removableRolls
+                .SelectMany(removableRoll => _grid
+                    .AdjacentAll(removableRoll.Pos)
+                    .Where(RollsFilter)).Distinct();
+            removableRolls = GetRemovableRolls(candidates).ToList();
         } while (removableRolls.Count > 0);
 
         return counter.ToString();
     }
 
-    private IEnumerable<GridItem<char>> GetRemovableRolls()
+    private static bool RollsFilter(GridItem<char> item)
     {
-        return _grid
-            .Flatten()
-            .Where(item => item.Value == PaperRoll
-                           && _grid.AdjacentAll(item)
-                               .Count(adjacent => adjacent.Value == PaperRoll) < Amount);
+        return item.Value == PaperRoll;
+    }
+
+    private IEnumerable<GridItem<char>> GetRemovableRolls(IEnumerable<GridItem<char>> candidates)
+    {
+        return candidates
+            .Where(item => _grid.AdjacentAll(item)
+                .Count(adjacent => adjacent.Value == PaperRoll) < Amount);
     }
 }
